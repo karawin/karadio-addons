b311c1f65e74929bc86958e9ad77284e704c4de5
karawin <jp@cocatrix.fr>
2017-05-20 12:20:34 +0200

    update u8glibConf.h comments

--- karadioU8STM32/u8glibConf.h
+++ karadioU8STM32/u8glibConf.h
@@ -119,6 +119,9 @@ Instruction to load the bootloader on stm32 (one time)
 http://www.stm32duino.com/viewtopic.php?f=32&t=413
 http://wiki.stm32duino.com/index.php?title=Uploading_a_sketch
 
+Board: Generic STM32F103C series
+Tools/variant set to 128K flash
+
 Libraries needed:
 u8glib for stm32 must be installed in  arduino IDE library;
 https://github.com/karawin/U8glib_Arduino
@@ -138,6 +141,7 @@ In line 47
 to
 #define WIRE_BUFSIZ 256  
 
+Define your tzo (time zone offset) at the beginning of the ino.
 
 
 --------
@@ -162,186 +166,4 @@ jpc 05/2017
 */
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*  *** Instruction for NOKIA 5110*********************************************
-        ---------------------------
-NOTES:
-Confirmed vendor:
-lcd: http://www.ebay.fr/itm/262558872574?_trksid=p2057872.m2749.l2649&ssPageName=STRK%3AMEBIDX%3AIT
-cpu: http://www.ebay.fr/itm/252409929753?_trksid=p2057872.m2749.l2649&ssPageName=STRK%3AMEBIDX%3AIT
-
-MiniPRO 3.3 v 8MHz Pins:
-#__      Function_________________________________
-
-RESET    Reset (not used)
-Tx  0    n/a (dedicated Serial Input)
-Rx  1    to the tx of the nodeMcu
-PIN 2    n/a
-PIN 3-7  Nokia Display  (specifics below)
-PIN 8    Activate changes to NOKIA contrast B0 --> BF GND and press RESET
-
-Raw: 3.3 to 16 VDC Max
-
-Nokia 5110 Graphic LCD Pinout:
-_______ Mini Pro____   _______ Nokia GLCD___
-#define PIN_SCE   7    LCD CE ....  Pin 2
-#define PIN_RESET 6    LCD RST .... Pin 1
-#define PIN_DC    5    LCD Dat/Com. Pin 3  (DC)
-#define PIN_SDIN  4    LCD SPIDat . Pin 4  (DIN)
-#define PIN_SCLK  3    LCD SPIClk . Pin 5
-
-//                     LCD Gnd .... Pin 8
-//                     LCD Vcc .... Pin 6   3.3 volts  from the minipro VCC
-//                     LCD Vled ... Pin 7   (100 to 300 Ohms to Gnd)
-
-Cable wiring between nodeMcu and Mini Pro
------------------------------------------
---NodeMcu--   --Mini Pro--
-Tx            Rx
-Gnd           Gnd
-VU            Raw
-
-Extra features:
----------------
-A Jumper between Nokia GLCD pin 7 and  100 ohm + Gnd to switch on or off the lcd light
-A7 ---| |--- 100 ohms --- GND
-A push button between PIN8 of mini pro and GND to ajust the contrast after a reset.
-- Press the switch, reset and wait for the right contrast (32 steps) .
-- Release the switch. Done.
-The contrast is saved in eprom.
-
-Software: 
-u8glib must be installed in library;
-In .../Arduino\libraries\U8glib\utility/u8g.h
- uncomment the following line 
-#define U8G_16BIT 1
-
-In file u8glibConf.h
-comment the current u8g
-uncomment:
-U8GLIB_PCD8544 u8g(PIN_SCLK, PIN_SDIN, PIN_SCE, PIN_DC, PIN_RESET); // SPI Com: SCK = 13, MOSI = 11, CS = 10, A0 = 9, Reset = 8
-
-Warning:
-The webradio serial must be set at 28800 b/s
-with sys.uart("28800") command
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-**** Instruction for 0.96" I2C IIC SPI Serial 128X64 White OLED LCD LED Display Module for Arduino *************************
-     ----------------------------------------------------------------------------------------------
-NOTES:
-Confirmed vendor:
-lcd: http://www.ebay.fr/itm/191909116703?_trksid=p2057872.m2749.l2649&ssPageName=STRK%3AMEBIDX%3AIT
-cpu: http://www.ebay.fr/itm/Pro-Mini-Atmega328-5V-16M-Micro-controller-Board-for-Arduino-Compatible-Nano-new-/331809989705?hash=item4d416aa449
-
-MiniPRO 5v 16MHz
- Pins:
-#__      Function_________________________________
-
-RESET    Reset (not used))
-Rx  1    to the tx of the nodeMcu
-Gnd      ground
-jp2: A4,A5	I2C lines
-Raw: 5 to 16 VDC Max
-
-Graphic LCD Pinout:
- Mini Pro____   _______  GLCD___
-JP2 A4             LCD SDA .... Pin 3
-P2  A5             LCD SCK .... Pin 4
-//                 LCD Gnd .... Pin 1
-//                 LCD Vcc .... Pin 2  5 volts  from the minipro VCC
-
-Depending of the pcb, A4 & A5 are on the opposite pins of the board, or on the pcb near A2 & A3 
-
-Cable wiring between nodeMcu and Mini Pro
------------------------------------------
---NodeMcu--   --Mini Pro--
-Tx            Rx
-Gnd           Gnd
-VU            Raw
-
-Software:
-u8glib must be installed in library;
-In .../Arduino\libraries\U8glib\utility/u8g.h
-uncomment the following line
-#define U8G_16BIT 1
-
-In file u8glibConf.h
-comment the current u8g
-uncomment:
-U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE|U8G_I2C_OPT_DEV_0);	// I2C / TWI
-
---------
-WARNING:
-The webradio serial must be set at 28800 b/s
---------
-WARNING:
----------
-The nodeMcu is a 3.3v device.
-DO NOT connect the Rx pin of the lcd to the Tx pin of the mini Pro
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-**** Instruction for the IR module *****************************************************************************************
-------------------------------------
-The model used here is from:
-https://www.aliexpress.com/item/Hot-Selling-1pcs-New-Infrared-IR-Wireless-Remote-Control-Module-Kits-For-Arduino-Wholesale/32334118062.html?spm=2114.13010608.0.0.XzmgYk
-or equivalent. Cost less than 1$
-
-- Based on NEC protocol; Built-in 1 x AG10 battery;
-- Remote control range: above 8m;
-- Wavelength: 940Nm;
-- Frequency: crystal oscillator: 455KHz; IR carrier frequency: 38KHz
-
-// obsolete-----------
-See Library at: https://github.com/shirriff/Arduino-IRremote
-Install the library found at  https://github.com/z3t0/Arduino-IRremote/releases/download/2.1.0/Arduino-IRremote-dev.zip
-Uncomment the #define IR at the beginning of the karadioU8glib.ino file.
-----------------------
-// new library
-Install the library from https://github.com/cyborg5/IRLib2
-Uncomment the #define IR  and the #ifdef IRLib2 at the beginning of the karadioU8glib.ino file.
-----------------------
-
-The IR receiver pins:
-From left to right (pin at bottom)
-Gnd VCC Signal
-The signal must be connected to
-#define PIN_IRRECV	11  for the old library or
-#define PIN_IRRECV  2  or 3 for the new library IRLib2
-It is the default In karadioU8glig.ino, you can change it
-
-In karadioU8glig.ino:
-//  Uncomment the following line to see the code of your remote control and report to the case the value
-//	    Serial.print("Protocol:");Serial.print(results.decode_type);Serial.print("  value:");Serial.println(results.value,HEX);
-If you want to use another remote, you can see the code of the keys and modify the switch case for the needed function.
-See IRremote.h  to add the protocol you need for the remote.
-
-The tx of the pro mini must be connected to the rx of the esp12 but
-Warning; the esp rx pin is not 5V tolerant, so if your pro mini is a 5v version we nned to adapt the level.
--------
-See
-http://www.instructables.com/id/Pi-Cubed-How-to-connect-a-33V-Raspberry-Pi-to-a-5V/
-
-The value of the resistors is not critical. Only th ration 1/3 2/3 must be respected 1k and 2k for example)
-
-I found that the ir is not very accurate, but it works.
-Enjoy.
-
-jpc 01/2017
-*/
 
