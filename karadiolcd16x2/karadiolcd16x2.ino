eb81bc634bc54ba8662d201745674c04a64380a9
karawin <jp@cocatrix.fr>
2018-05-09 10:26:15 +0200

    16x2 update for scrolling

--- karadiolcd16x2/karadiolcd16x2.ino
+++ karadiolcd16x2/karadiolcd16x2.ino
@@ -4,7 +4,7 @@
   Karadiolcd16x2.ino
 
   Edited by Paul Sijben May 2018.
-
+  
 */
 // include the library code:
 #include <LiquidCrystal.h>
@@ -21,7 +21,7 @@
  *      ends to +5V and ground
  *      wiper to LCD VO pin (pin 3)
 */
-
+ 
 #define version 2.0
 
 #define ChUpPin 10
@@ -30,7 +30,7 @@
 #define StopPin 7
 
 // constants
-const int  BAUD            = 28800;  // use the telnet interface on the esp32 to set the baudrate there sys.uart("28800")
+const int  BAUD            = 19200;  // use the telnet interface on the esp32 to set the baudrate there sys.uart("19200")
 
 const byte ContrastPin     = 8;     // D8 low activates the Contrast adjustment
 
@@ -38,14 +38,15 @@ const byte ContrastPin     = 8;     // D8 low activates the Contrast adjustment
    char  msg[]       = {"Karadio lcd"}; //
    char  msg1[]      = {"(c) KaraWin"}; //
    char  blank[]     = {"                "};
+   char  msg2[]      = {"web interface:  "};
 // Karadio specific data
 #define BUFLEN  200
-#define LINES    2
+#define LINES	2
 char line[BUFLEN]; // receive buffer
 char station[BUFLEN]; //received station
-char title[BUFLEN];    // received title
+char title[BUFLEN];	// received title
 char nameset[BUFLEN];
-char* lline[LINES] ; // array of ptr of n lines
+char* lline[LINES] ; // array of ptr of n lines 
 int  iline[LINES] ; //array of index for scrolling
 byte  tline[LINES] ;
 char* ici;
@@ -58,8 +59,8 @@ int laststateUp=LOW;
 int laststateDown=LOW;
 int laststatePlay=LOW;
 int laststateStop=LOW;
-int buttonState = 0;
-int x = 16;        //Width
+int buttonState = 0; 
+int x = 16;		//Width
 char temp;
 
  // initialize the library with the numbers of the interface pins
@@ -71,12 +72,12 @@ void clearAll()
 {
       title[0] = 0;
       station[0]=0;
-      for (int i=0;i<LINES;i++)
-      {
+	  for (int i=0;i<LINES;i++)
+	  {
       lline[i] = NULL;iline[i] = 0;tline[i] = 0;
        lcd.setCursor(0, i);
        lcd.print(blank);
-      }
+	  }
 }
 ////////////////////////////////////////
 void cleartitle()
@@ -85,11 +86,11 @@ void cleartitle()
      for (int i = 1 ;i<LINES;i++)  // clear lines
      {
         lline[i] = NULL;
-          iline[i] = 0;
-          tline[i] = 0;
+	      iline[i] = 0;
+	      tline[i] = 0; 
         lcd.setCursor(0, i);
         lcd.print(blank);
-     }
+     }  
      draw();
 }
 
@@ -104,35 +105,44 @@ void parse(char* line)
  ////// Meta title
    if ((ici=strstr(line,"META#: ")) != NULL)
    {
-     cleartitle();
+     cleartitle(); 
      //remove station name if present
      if ((ici2=strstr(line,station)) != NULL)
      {
       ici+=strlen(station);
      }
-     strcpy(title,ici+7);
-     lline[1] = title;
-     draw();
-   } else
-
+     strcpy(title,ici+7);    
+	 lline[1] = title; 
+	 draw();	
+   } else 
+ 
  ////// ICY0 station name
    if ((ici=strstr(line,"ICY0#: ")) != NULL)
    {
       int len;
       clearAll();
-      if (strlen(ici+7) == 0) strcpy (station,nameset);
+	  if (strlen(ici+7) == 0) strcpy (station,nameset);
       else strcpy(station,ici+7);
-      lline[0] = station;
-      draw();
+	  lline[0] = station;
+	  draw();
    } else
- ////// STOPPED
+ ////// STOPPED  
    if ((ici=strstr(line,"STOPPED")) != NULL)
    {
        cleartitle();
        strcpy(title,"STOPPED");
-       lline[1] = title;
-           draw();
-   }
+	   lline[1] = title;
+	       draw();	
+   } else
+   // advertise web interface   
+   if ((ici=strstr(line,"IP: ")) != NULL)
+   {
+     clearAll(); 
+     lline[0]=msg2;
+     strcpy(title,ici+4);    
+   lline[1] = title; 
+   draw();  
+   }  
 
 }
 
@@ -143,14 +153,14 @@ void setup(void) {
     char oip[20];
    Serial.begin(BAUD);
    while (!Serial) { ;}
-Serial.println("Arduino Ready");
+  Serial.println("Arduino Ready");
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   pinMode(13, OUTPUT); // led
-
+   
    //Serial.println(F("Free RAM available:")) ;
    //Serial.print(freeRam());
-    lcd.clear();
+	lcd.clear();
   lcd.noAutoscroll() ;
 
   lcd.print(msg);
@@ -161,10 +171,10 @@ Serial.println("Arduino Ready");
   pinMode(ChDownPin, INPUT);
   pinMode(PlayPin, INPUT);
   pinMode(StopPin, INPUT);
-
-  delay(3000);
+  
+  delay(3000); 
   clearAll();
-
+  
 }
 
 ////////////////////////////////////////
@@ -174,93 +184,101 @@ void serial()
     char temp;
     while ((temp=Serial.read()) != -1)
     {
-        switch (temp)
-        {
-            case '\n' : if (index == 0) break;
-            case '\r' :
-                line[index] = 0; // end of string
-                index = 0;
-                parse(line);
-                break;
-            default : // put the received char in line
-                if (index>BUFLEN-1) break; // small memory so small buffer
-                line[index++] = temp;
-        }
+	    switch (temp)
+	    {
+		    case '\n' : if (index == 0) break;
+		    case '\r' :
+				line[index] = 0; // end of string
+				index = 0;
+				parse(line);
+				break;
+		    default : // put the received char in line
+				if (index>BUFLEN-1) break; // small memory so small buffer
+				line[index++] = temp;
+	    }
     }
 }
+
 
 ////////////////////////////////////////
 // draw all lines
 void draw()
 {
     lcd.clear();
-        for (int i = 0;i < LINES;i++)
-        {
-            serial();
+		for (int i = LINES-1;i >=0;i--)
+		{
+			serial();
       lcd.setCursor(0, i);
-            if ((lline[i] != NULL)) lcd.print(lline[i]+iline[i]);
+			if ((lline[i] != NULL)) lcd.print(lline[i]+iline[i]);
       delay(1);
-        }
+		}
+    
 }
 ////////////////////////////////////////
 // scroll each line
 void scroll()
 {
 unsigned len;
-    for (int i = 0;i < LINES;i++)
-    {
-       if (tline[i]>0)
-       {
-         if (tline[i] == 4) iline[i]= 0;
-         tline[i]--;
-       }
-       else
-       {
-           len = strlen(lline[i]+(iline[i]));
-           if (len > x) iline[i]++;
-          else
-            {tline[i] = 6;}
-       }
-    }
-    draw();
+int dodraw=0;
+
+	for (int i = 0;i < LINES;i++)
+	{   
+	   if (tline[i]>0) 
+	   {
+	     if (tline[i] == 4) { iline[i]= 0;dodraw=1; }
+	     tline[i]--;		
+	   } 
+	   else
+	   {
+		   len = strlen(lline[i]+(iline[i]));
+		   if (len > x) {
+		     iline[i]++;
+         dodraw=1;
+		    }
+		   else 
+			  {tline[i] = 6;}
+	   }
+	}
+ if (dodraw==1)	draw();
+  
 }
 
 ////////////////////////////////////////
 void loop(void) {
-    serial();
+	serial();
     if (loopcount++ == 0x3fff)
     {
-        loopcount = 0;
-        //if (++scrl%6 == 0) digitalWrite(13, HIGH);
-        scroll();
-        //digitalWrite(13, LOW);
+	    loopcount = 0;
+		//if (++scrl%6 == 0) digitalWrite(13, HIGH);	
+		scroll();	
+		//digitalWrite(13, LOW);
     }
     // yes this can be done in a nicer way, but for 4 buttons I am not going to bother
     buttonState = digitalRead(ChUpPin);
     if (buttonState!= laststateUp) {
       laststateUp=buttonState;
-      if (buttonState == HIGH) {
+      if (buttonState == HIGH) { 
         Serial.println("cli.next");
       }
     }
     buttonState = digitalRead(ChDownPin);
     if (buttonState!= laststateDown) {
       laststateDown=buttonState;
-      if (buttonState == HIGH) {
+      if (buttonState == HIGH) { 
         Serial.println("cli.prev");
       }
     }
     buttonState = digitalRead(PlayPin);
     if (buttonState!= laststatePlay) {
       laststatePlay=buttonState;
-      if (buttonState == HIGH) {
+      if (buttonState == HIGH) { 
         Serial.println("cli.start");
       }
     }
     buttonState = digitalRead(StopPin);
     if (buttonState!= laststateStop) {
       laststateStop=buttonState;
-      if (buttonState == HIGH) {
+      if (buttonState == HIGH) { 
         Serial.println("cli.stop");
       }
     }
